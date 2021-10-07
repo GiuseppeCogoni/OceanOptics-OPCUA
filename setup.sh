57abfb836d0f0de6adf3b2b9b329cfe50d025cc0
GiuseppeCogoni <giuseppe.cogoni@pfizer.com>
2021-10-07 15:44:35 -0400

    working on the shell script

--- setup.sh
+++ setup.sh
@@ -7,9 +7,21 @@ pwd=$(pwd)
 # This part checks if the repo folder is already available
 if [ -d $pwd"/OceanOptics-OPCUA" ]
 then
-    echo "Folder already exists!"
+  echo "Folder already exists!"
 else
-    git clone https://github.com/GiuseppeCogoni/OceanOptics-OPCUA.git
+  git clone https://github.com/GiuseppeCogoni/OceanOptics-OPCUA.git
+fi
+
+sudo apt-get update && sudo apt-get -y upgrade
+# Checking if docker is installed
+if [[ which docker && docker --version ]]; then
+  echo "Docker already installed and up to date!"
+
+else
+  echo "Installing Docker..."
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh get-docker.sh
+  sudo usermod -aG docker pi
 fi
 
 # Changing directory to the downloaded repo
@@ -22,10 +34,16 @@ docker image inspect ocean_ua:latest >/dev/null 2>&1 && im="yes" || im="no"
 
 if [ $im == "no" ]
 then
-    echo "Creating docker image..."
-    docker build -t ocean_ua .
+  echo "Creating docker image..."
+  docker build -t ocean_ua .
 else
-    echo "Docker image already available!"
+  echo "Docker image already available!"
 fi
 
+# run docker container
+echo "Running the docker container..."
 
+docker run -it --restart unless-stopped \ 
+        -d -p 4840:4840 \
+        --privileged -v "/dev/*:/dev/*" \
+        --name ocean_ua ocean_ua:latest
